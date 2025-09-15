758ae471d87e44d47059f4f1c82454ea4b297120
jemrules <evern.mccullough@comcast.net>
2025-09-15 17:38:32 -0600

    Added MyFIRSTJavaOpMode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class MyFIRSTJavaOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+    }
+}
