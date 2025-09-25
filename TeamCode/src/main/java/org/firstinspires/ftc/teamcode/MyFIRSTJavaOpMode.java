76400481228cbbc2371712e10e52ab95326dbd3b
jemrules <jemrulesreal@gmail.com>
2025-09-25 07:55:23 -0700

    Movde Movement class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.teamcode.robot.Movement;
 
 import java.util.Vector;
 
@@ -18,59 +19,12 @@ import static java.lang.Math.*;
      Left HD Motor -> left_motor
      Right HD Motor -> right_motor
 
-* */
+*/
 @TeleOp
-public class MyFIRSTJavaOpMode extends OpMode {
-    boolean Autonomous=false;
-
-    public class Movement {
-        public VectorF position;
-        public double targetHeading;
-        public IMU imu;
-        public Movement(VectorF default_position,double default_heading) {
-            // Set the default position and default heading
-            position=default_position;
-            targetHeading=default_heading;
-
-            // CONTROL HUB ORIENTATION SETTINGS
-            RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-            RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
-            RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection,usbDirection);
-
-            imu=hardwareMap.get(IMU.class,"imu");
-            imu.initialize(new IMU.Parameters(orientationOnRobot));
-        }
-        // Set default_heading to 0.0 if default_heading isn't given
-        public Movement(VectorF default_position) {this(default_position,0.0);}
-        public void RobotStart() {
-            imu.resetYaw();
-        }
-        // Get heading from the IMU (Control Hub)
-        public double getHeading() {
-            return 0.0;
-        }
-        public void setHeading(double target_heading) {
-
-        }
-        public void lookAt(VectorF target_position) {
-
-        }
-        // Drive in that direction
-        public void driveTo(VectorF target_vector) {
-
-        }
-        public void driveTo(double orientation,double distance) {
-            driveTo(new VectorF(
-                    (float)(cos(orientation)*distance),
-                    (float)(sin(orientation)*distance)));
-        }
-    }
+public class MyFIRSTJavaOpMode extends LinearOpMode {
+    public Movement
     @Override
-    public void init() {
-        telemetry.addData("Hello","World");
-    }
-    @Override
-    public void loop() {
-
+    public void runOpMode() {
+        
     }
 }
