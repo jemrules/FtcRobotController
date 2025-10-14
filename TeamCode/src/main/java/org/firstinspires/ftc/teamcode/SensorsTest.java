16836dc70f498045694b17c0e1f154734165a384
jemrules <evern.mccullough@comcast.net>
2025-10-14 16:36:49 -0600

    Webcam can now detect april tags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorsTest.java
@@ -9,9 +9,24 @@ import org.firstinspires.ftc.teamcode.robot.Sensors;
 
 import static java.lang.Math.PI;
 import static java.lang.Math.toRadians;
+@TeleOp
 public class SensorsTest extends LinearOpMode {
+    public Sensors robotSensors;
     @Override
     public void runOpMode() {
-
+        robotSensors = new Sensors(
+                hardwareMap);
+        robotSensors.startStream();
+        while (opModeInInit()) {
+            telemetry.addData("Status","Ready to Start");
+            telemetry.update();
+        }
+        while (opModeIsActive()) {
+            robotSensors.detectTags();
+            robotSensors.telemetryOutput(telemetry);
+            telemetry.update();
+            sleep(20);
+        }
+        robotSensors.visionPortal.close();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Sensors.java
@@ -2,21 +2,47 @@ package org.firstinspires.ftc.teamcode.robot;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
 
 public class Sensors {
     public AprilTagProcessor aprilTag;
 
     public VisionPortal visionPortal;
+    public ArrayList<AprilTagDetection> currentDetections;
     public Sensors(HardwareMap hardwareMap) {
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
         // Using Webcam
         visionPortal = VisionPortal.easyCreateWithDefaults(
-                hardwareMap.get(WebcamName.class,"Webcam 1")
-        );
+                hardwareMap.get(WebcamName.class,"Webcam 1"),aprilTag);
     }
-    public void updateRobot() {
+    public void detectTags() {
+        currentDetections = aprilTag.getDetections();
+    }
+    public void startStream() {
+        visionPortal.resumeStreaming();
+    }
+    public void stopStream() {
+        visionPortal.stopStreaming();
+    }
 
+    public void telemetryOutput(Telemetry telemetry) {
+        telemetry.addData("# Tags Detected",currentDetections.size());
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }
     }
 }
