e65adead33e6661802c163e747f91dcefc9bfe36
jemrules <evern.mccullough@comcast.net>
2025-10-21 14:53:57 -0600

    added conversion

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/conversion.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import static java.lang.Math.PI;
+
+
+public class conversion {
+    public static float in2m(float value) {return value/39.3701f;}
+    public static double in2m(double value) {return value/39.3701;}
+
+    public static float deg2rad(float value) {return value*(float) PI/180.0f;}
+    public static double deg2rad(double value) {return value*PI/180.0;}
+}
