840e02eab59b2dc75607e6ba00f976d65bb42a30
jemrules <evern.mccullough@comcast.net>
2025-10-21 16:15:08 -0600

    Revert "Optimized Angle Subtraction Speed"
    This reverts commit a814ebf7dd5bcd6f9082a1820a257cae0f32e712.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TrigMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TrigMath.java
@@ -3,18 +3,14 @@ package org.firstinspires.ftc.teamcode.util;
 import static java.lang.Math.atan2;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
-import static java.lang.Math.PI;
 import static java.lang.Math.tan;
 
 public class TrigMath {
     public static <T extends Number> double subtractAngles(T a1, T a2) {
         double A1=(double)a1;
         double A2=(double)a2;
-        // Using individual variables is faster than compacting it into one
-        double cos1=cos(A1);double sin1=sin(A1);
-        double cos2=cos(A2);double sin2=sin(A2);
-        double X=cos1*cos2+sin1*sin2;
-        double Y=sin1*cos2-cos1*sin2;
+        double X=cos(A1)*cos(A2)+sin(A1)*sin(A2);
+        double Y=sin(A1)*cos(A2)-cos(A1)*sin(A2);
         return atan2(Y,X);
     }
 }
