9a139f280550be684e6be1d495c0649b1c87124e
jemrules <evern.mccullough@comcast.net>
2025-10-21 15:55:54 -0600

    added subtractAngles function to easily subtract two angles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TrigMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TrigMath.java
@@ -1,5 +1,16 @@
 package org.firstinspires.ftc.teamcode.util;
 
-public class TrigMath {
+import static java.lang.Math.atan2;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.tan;
 
+public class TrigMath {
+    public static <T extends Number> double subtractAngles(T a1, T a2) {
+        double A1=(double)a1;
+        double A2=(double)a2;
+        double X=cos(A1)*cos(A2)+sin(A1)*sin(A2);
+        double Y=sin(A1)*cos(A2)-cos(A1)*sin(A2);
+        return atan2(Y,X);
+    }
 }
