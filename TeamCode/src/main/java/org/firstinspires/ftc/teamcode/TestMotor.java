7881463700053b20755e67f1170eec14463b45fe
jemrules <evern.mccullough@comcast.net>
2025-10-28 16:44:52 -0600

    Added motor tester script

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotor.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class TestMotor extends LinearOpMode {
+    public DcMotor motor;
+    @Override
+    public void runOpMode() {
+        motor=hardwareMap.get(DcMotor.class,"test_motor");
+        while (opModeInInit()) {
+            telemetry.addData("Status","Ready to Start");
+            telemetry.update();
+        }
+        while (opModeIsActive()) {
+            motor.setPower(gamepad1.right_stick_y);
+        }
+    }
+}
