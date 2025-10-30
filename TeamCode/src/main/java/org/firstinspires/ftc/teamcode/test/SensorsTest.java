d57ecfc13e5c54c788014b15f5a8ef451a49b11d
jemrules <evern.mccullough@comcast.net>
2025-10-30 15:14:05 -0600

    Moved Test Files to test folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/SensorsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/SensorsTest.java
@@ -1,14 +1,10 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
-import org.firstinspires.ftc.teamcode.robot.Movement;
 import org.firstinspires.ftc.teamcode.robot.Sensors;
 
-import static java.lang.Math.PI;
-import static java.lang.Math.toRadians;
 @TeleOp
 public class SensorsTest extends LinearOpMode {
     public Sensors robotSensors;
