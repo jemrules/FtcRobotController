56742ed3b965dabf5a28e93cb60890b6d0e45356
jemrules <evern.mccullough@comcast.net>
2025-11-06 16:02:27 -0700

    New ManualDrive OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDrive.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class ManualDrive extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+    }
+}
