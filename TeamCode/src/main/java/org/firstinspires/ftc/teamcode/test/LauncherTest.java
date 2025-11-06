fe2455646d68ea20c45f3e1b4ef663f545413bf7
jemrules <evern.mccullough@comcast.net>
2025-11-06 16:12:49 -0700

    Created Launcher Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/LauncherTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.robot.Launcher;
+
+@TeleOp
+public class LauncherTest  extends LinearOpMode {
+    Launcher launcher;
+    @Override
+    public void runOpMode() {
+        launcher=new Launcher(hardwareMap);
+        // Wait until the play button is pressed
+        while (opModeInInit()) {
+            telemetry.addData("Status","Ready to Start");
+            telemetry.update();
+        }
+        // Send to the robot movement controller Init has ended
+        launcher.RobotStart();
+
+        while (opModeIsActive()) {
+            launcher.setRPS(gamepad1.right_stick_y);
+            launcher.UpdateRobot();
+            launcher.debugData(telemetry);
+        }
+    }
+}
