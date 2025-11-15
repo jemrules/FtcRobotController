5baeef634bfb0139cb86137d1ac7d8ce51d74e18
jemrules <evern.mccullough@comcast.net>
2025-11-15 01:11:50 -0700

    Disabled Test OpModes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/OdometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/OdometryTest.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode.test;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
+@Disabled
 @TeleOp
 public class OdometryTest  extends LinearOpMode {
     @Override
