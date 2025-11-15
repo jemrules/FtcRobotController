d0d4fba3b889b62cdec64724920abea964f9b895
jemrules <evern.mccullough@comcast.net>
2025-11-15 01:12:07 -0700

    Created Quick Simple Auto Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.teamcode.robot.Launcher;
 import org.firstinspires.ftc.teamcode.robot.Movement;
 import org.firstinspires.ftc.teamcode.robot.Sensors;
@@ -15,6 +17,29 @@ public class AutoDrive extends LinearOpMode {
     public Sensors robotSensors;
     @Override
     public void runOpMode() {
-
+        launcher=new Launcher(hardwareMap);
+        robotMovement = new Movement(
+                new VectorF(0.0f, 0.0f, 0.0f), // Set default position to 0,0,0
+                hardwareMap); // Pass the ability to interact with hardware
+        robotSensors = new Sensors(
+                hardwareMap);
+        double wheel_max_rps=robotMovement.left_motor.getMotorType().getMaxRPM()/60.0;
+        // Wait until the play button is pressed
+        while (opModeInInit()) {
+            telemetry.addData("Status", "Ready to Start");
+            telemetry.update();
+        }
+        robotMovement.RobotStart();
+        launcher.RobotStart();
+        ElapsedTime drive_time = new ElapsedTime();
+        drive_time.reset();
+        while (opModeIsActive()) {
+            if (drive_time.seconds()<3.0) {
+                robotMovement.movement_vector.put(1,-0.5f);
+            } else {
+                robotMovement.movement_vector.put(1,0.0f);
+                assert drive_time.seconds()<3.0 && robotMovement.movement_vector.get(1)==0.0f : "Robot Vector not resetting to 0.0f";
+            }
+        }
     }
 }
